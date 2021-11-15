a7999ba2bef72674975478efeaa5b8e0a39edf83
I_Agreed <48776053+I-Agreed@users.noreply.github.com>
2021-11-15 14:36:07 +1100

    added trails and made it work

--- source/Planet.hpp
+++ source/Planet.hpp
@@ -1,6 +1,8 @@
 #pragma once
 #include <SFML/System.hpp>
 #include <SFML/Graphics.hpp>
+#include <deque>
+using namespace std;
 
 struct Planet {
 	sf::Vector2f pos;
@@ -8,13 +10,19 @@ struct Planet {
 
 	float mass = 0; // physics mass
 	float size = 0; // visual size (circle radius)
+	bool fixed = false;
+	deque<sf::Vector2f> trail;
+	bool createTrail = true;
 	sf::Color colour;
 
-	Planet(double x = 0, double y = 0, double vx = 0, double vy = 0, double m = 0) {
+	Planet(double x = 0, double y = 0, double vx = 0, double vy = 0, double m = 0, bool f = false, bool t = true) {
 		pos = sf::Vector2f(x, y);
 		vel = sf::Vector2f(vx, vy);
 		mass = m;
-		size = 2;
+		size = pow(m, 1.0/3.0);
+		colour = sf::Color(255, 255, 255);
+		fixed = f;
+		createTrail = t;
 	}
 };
 
--- source/main.cpp
+++ source/main.cpp
@@ -6,42 +6,78 @@ using namespace std;
 
 const float G = 1;
 const int FPS = 60;
+const float PHYS_SCALE = 0.4; // physics time scale
+const int WIDTH = 800; // screen width
+const int HEIGHT = 600; // screen height
+const int TRAIL_LENGTH = 120; // number of points in trail
+const int TRAIL_WIDTH = 5; // maximum trail width
+
+sf::Vector2f cameraPos(WIDTH/2, HEIGHT/2);
 
 vector<Planet> planets;
 sf::Clock frameClock;
 
+float length(sf::Vector2f p) {
+	return sqrtf(pow(p.x, 2) + pow(p.y, 2));
+}
 
 float distance(sf::Vector2f p1, sf::Vector2f p2) {
 	sf::Vector2f p3 = p2 - p1;
-	return sqrtf(pow(p3.x, 2) + pow(p3.x, 2));
+	return sqrtf(pow(p3.x, 2) + pow(p3.y, 2));
+}
+
+sf::Vector2f normalize(sf::Vector2f p) { // returns direction vector from p1 to p2
+	return p / length(p);
 }
 
 sf::Vector2f direction(sf::Vector2f p1, sf::Vector2f p2) { // returns direction vector from p1 to p2
 	sf::Vector2f p3 = p2 - p1;
-	return p3 * distance(p1, p2);
+	return p3 / length(p3);
 }
 
 void update() {
 	for (int i = 0; i < planets.size(); i++) {
-		sf::Vector2f force = sf::Vector2f(0,0);
-		for (int j = 0; j < planets.size(); j++) {
-			if (j != i) {
-				force += planets[j].mass * direction(planets[i].pos, planets[j].pos) / powf(distance(planets[i].pos, planets[j].pos), 2);
+		if (!planets[i].fixed) {
+			sf::Vector2f force = sf::Vector2f(0,0);
+			for (int j = 0; j < planets.size(); j++) {
+				if (j != i) {
+					force += planets[j].mass * direction(planets[i].pos, planets[j].pos) / powf(distance(planets[i].pos, planets[j].pos), 2);
+				}
 			}
+			planets[i].vel += G * force * PHYS_SCALE;
 		}
-		planets[i].vel += G * force;
 	}
 
 	for (int i = 0; i < planets.size(); i++) {
-		planets[i].pos += planets[i].vel;
-	}
+		if (planets[i].createTrail) {
+			float trailSize = (-1/(length(planets[i].vel)/2+1) + 1) * TRAIL_WIDTH; // when moving faster, the trail is thicker
+			sf::Vector2f normal = normalize(sf::Vector2f(-planets[i].vel.y, planets[i].vel.x));
+			planets[i].trail.push_back(planets[i].pos + normal*trailSize);
+			planets[i].trail.push_back(planets[i].pos - normal*trailSize);
+			if (planets[i].trail.size() > TRAIL_LENGTH * 2) {
+				planets[i].trail.pop_front();
+				planets[i].trail.pop_front(); // each trail 'point' is actually two points
+			}
+		}
+		planets[i].pos += planets[i].vel * PHYS_SCALE;
+	} 
 }
 
 void draw_planet(Planet p, sf::RenderWindow* win) {
+	if (p.createTrail) {
+		sf::VertexArray line(sf::TriangleStrip, p.trail.size());
+		for (int i = 0; i < p.trail.size(); i++) {
+			line[i].position = p.trail[i] + cameraPos;
+			float colourScale = (i/(p.trail.size()-1.f));
+			line[i].color = sf::Color(p.colour.r * colourScale, p.colour.g *  colourScale,
+									  p.colour.b *  colourScale, p.colour.a *  colourScale);
+		}
+		win->draw(line);
+	}
 	sf::CircleShape circle;
 	circle.setRadius(p.size);
 	circle.setFillColor(p.colour);
-	circle.setPosition(p.pos - sf::Vector2f(p.size, p.size));
+	circle.setPosition(p.pos - sf::Vector2f(p.size, p.size) + cameraPos);
 
 	win->draw(circle);
 }
@@ -55,6 +91,9 @@ void draw_all_planets(sf::RenderWindow* win) {
 int main() {
 	sf::RenderWindow window(sf::VideoMode(800, 600), "SFML window");
 	
+	planets.push_back(Planet(0, 0, 0, 0, 10000, true));
+	planets.push_back(Planet(200, 0, 0, 4, 100));
+
 	while (window.isOpen()) {
 		frameClock.getElapsedTime();
 		sf::sleep(sf::seconds(1.0 / FPS) - frameClock.getElapsedTime());
