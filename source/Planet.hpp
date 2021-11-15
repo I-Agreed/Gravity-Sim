1f7e973dc5c4cedc216f6996b37711f8c644ea24
I_Agreed <48776053+I-Agreed@users.noreply.github.com>
2021-11-15 18:11:03 +1100

    cool stuff, but trails stopped working somehow

--- source/Planet.hpp
+++ source/Planet.hpp
@@ -15,12 +15,12 @@ struct Planet {
 	bool createTrail = true;
 	sf::Color colour;
 
-	Planet(double x = 0, double y = 0, double vx = 0, double vy = 0, double m = 0, bool f = false, bool t = true) {
+	Planet(float x = 0, float y = 0, float vx = 0, float vy = 0, float m = 0, bool f = false, bool t = true, sf::Color c = sf::Color(255, 255, 255)) {
 		pos = sf::Vector2f(x, y);
 		vel = sf::Vector2f(vx, vy);
 		mass = m;
 		size = pow(m, 1.0/3.0);
-		colour = sf::Color(255, 255, 255);
+		colour = c;
 		fixed = f;
 		createTrail = t;
 	}
--- source/main.cpp
+++ source/main.cpp
@@ -2,6 +2,8 @@
 #include <vector>
 #include <math.h>
 #include "Planet.hpp"
+
+#define PI 3.14159265
 using namespace std;
 
 const float G = 1;
@@ -9,13 +11,15 @@ const int FPS = 60;
 const float PHYS_SCALE = 0.4; // physics time scale
 const int WIDTH = 800; // screen width
 const int HEIGHT = 600; // screen height
-const int TRAIL_LENGTH = 120; // number of points in trail
+const int TRAIL_LENGTH = 90; // number of points in trail
 const int TRAIL_WIDTH = 5; // maximum trail width
 
 sf::Vector2f cameraPos(WIDTH/2, HEIGHT/2);
 
 vector<Planet> planets;
 sf::Clock frameClock;
+bool lastMouseDown = false;
+sf::Vector2i lastMousePos;
 
 float length(sf::Vector2f p) {
 	return sqrtf(pow(p.x, 2) + pow(p.y, 2));
@@ -35,6 +39,17 @@ sf::Vector2f direction(sf::Vector2f p1, sf::Vector2f p2) { // returns direction 
 	return p3 / length(p3);
 }
 
+void add_trail(Planet p) {
+	float trailSize = (-1/(length(p.vel)/2+1) + 1) * TRAIL_WIDTH; // when moving faster, the trail is thicker
+	sf::Vector2f normal = normalize(sf::Vector2f(-p.vel.y, p.vel.x));
+	p.trail.push_back(p.pos + normal*trailSize);
+	p.trail.push_back(p.pos - normal*trailSize);
+	if (p.trail.size() > TRAIL_LENGTH * 2) {
+		p.trail.pop_front();
+		p.trail.pop_front(); // each trail 'point' is actually two points
+	}
+}
+
 void update() {
 	for (int i = 0; i < planets.size(); i++) {
 		if (!planets[i].fixed) {
@@ -50,30 +65,25 @@ void update() {
 
 	for (int i = 0; i < planets.size(); i++) {
 		if (planets[i].createTrail) {
-			float trailSize = (-1/(length(planets[i].vel)/2+1) + 1) * TRAIL_WIDTH; // when moving faster, the trail is thicker
-			sf::Vector2f normal = normalize(sf::Vector2f(-planets[i].vel.y, planets[i].vel.x));
-			planets[i].trail.push_back(planets[i].pos + normal*trailSize);
-			planets[i].trail.push_back(planets[i].pos - normal*trailSize);
-			if (planets[i].trail.size() > TRAIL_LENGTH * 2) {
-				planets[i].trail.pop_front();
-				planets[i].trail.pop_front(); // each trail 'point' is actually two points
-			}
+			add_trail(planets[i]);
 		}
 		planets[i].pos += planets[i].vel * PHYS_SCALE;
 	} 
 }
 
-void draw_planet(Planet p, sf::RenderWindow* win) {
+void draw_trail(Planet p, sf::RenderWindow* win) {
 	if (p.createTrail) {
 		sf::VertexArray line(sf::TriangleStrip, p.trail.size());
 		for (int i = 0; i < p.trail.size(); i++) {
 			line[i].position = p.trail[i] + cameraPos;
 			float colourScale = (i/(p.trail.size()-1.f));
-			line[i].color = sf::Color(p.colour.r * colourScale, p.colour.g *  colourScale,
-									  p.colour.b *  colourScale, p.colour.a *  colourScale);
+			line[i].color = sf::Color(p.colour.r, p.colour.g, p.colour.b, p.colour.a *  colourScale);
 		}
 		win->draw(line);
 	}
+}
+
+void draw_planet(Planet p, sf::RenderWindow* win) {
 	sf::CircleShape circle;
 	circle.setRadius(p.size);
 	circle.setFillColor(p.colour);
@@ -84,15 +94,29 @@ void draw_planet(Planet p, sf::RenderWindow* win) {
 
 void draw_all_planets(sf::RenderWindow* win) {
 	for (Planet p : planets) {
+		draw_trail(p, win);
+	}
+	for (Planet p : planets) {
 		draw_planet(p, win);
 	}
 }
 
+void handle_mouse_move(sf::Event e, sf::RenderWindow* win) {
+	sf::Vector2i pos = sf::Mouse::getPosition(*win);
+	if (sf::Mouse::isButtonPressed(sf::Mouse::Left)) {
+			sf::Vector2i change = pos - lastMousePos;
+			cameraPos += sf::Vector2f(change.x, change.y);
+	}
+	lastMousePos = pos;
+}
+
 int main() {
-	sf::RenderWindow window(sf::VideoMode(800, 600), "SFML window");
+	sf::RenderWindow window(sf::VideoMode(800, 600), "Gravity Sim");
 	
 	planets.push_back(Planet(0, 0, 0, 0, 10000, true));
-	planets.push_back(Planet(200, 0, 0, 4, 100));
+	planets.push_back(Planet(100, 0, 0, -8, 100, false, true, sf::Color(255, 0, 0)));
+	planets.push_back(Planet(-50, 50*sqrt(3), 4*sqrt(3), 4, 100, false, true, sf::Color(0, 255, 0)));
+	planets.push_back(Planet(-50, -50*sqrt(3), -4*sqrt(3), 4, 100, false, true, sf::Color(0, 0, 255)));
 
 	while (window.isOpen()) {
 		frameClock.getElapsedTime();
@@ -100,8 +124,8 @@ int main() {
 		sf::Event event;
 		while (window.pollEvent(event)) {
 			if (event.type == sf::Event::Closed) window.close();
+			if (event.type == sf::Event::MouseMoved) handle_mouse_move(event, &window);
 		}
-
 		window.clear();
 		draw_all_planets(&window);
 		window.display();
